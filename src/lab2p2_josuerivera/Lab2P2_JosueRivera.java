9afa33971836343e686049d8b95fa6d1642f1679
JosueRivera <josuerive20052323@gmail.com>
2024-01-26 15:07:48 -0600

    entrega2

--- src/lab2p2_josuerivera/Lab2P2_JosueRivera.java
+++ src/lab2p2_josuerivera/Lab2P2_JosueRivera.java
@@ -4,6 +4,7 @@
  */
 package lab2p2_josuerivera;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,11 +13,20 @@ import java.util.Scanner;
  */
 public class Lab2P2_JosueRivera {
 static Scanner sc= new Scanner(System.in);
+static Scanner cs = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int cont= 0;
+        ArrayList <Usuario> usuarios = new ArrayList();
+        Usuario user = new Usuario("Josue","Jroberto66","Estudiantes");
+        Usuario user_2 = new Usuario("Diego","D_Rosales","Maestro");
+        Usuario user_3 = new Usuario ("Luis","LFCS","Biblotecario");
+        usuarios.add(user);
+        usuarios.add(user_2);
+        usuarios.add(user_3);
         System.out.println("////MENU/////");
         System.out.println("1. iniciar seccion");
         System.out.println("2. Salir");
@@ -28,8 +38,35 @@ static Scanner sc= new Scanner(System.in);
             respuesta = sc.nextInt();
         }
         while(respuesta != 2){
-            System.out.println("ok1");
-            break;
+            System.out.println("Ingrese nombre de usuario:");
+            String nombre = cs.next();
+            System.out.println("Ingrese la contrase;;");
+            String contraseña = cs.next();
+            for (int i = 0; i < usuarios.size(); i++) {
+                if (nombre.equals(usuarios.get(i).getName())){
+                    if (contraseña.equals(usuarios.get(i).getContraseña())){
+                        cont = 1;
+                    }
+                    else{
+                        continue;
+                    }
+                        
+                }
+                else{
+                    continue;
+                }
+            }
+            if (cont == 1){
+                cont = 0;
+                
+            }
+            else{
+                System.out.println("////MENU/////");
+                System.out.println("1. iniciar seccion");
+                System.out.println("2. Salir");
+                respuesta = sc.nextInt();
+            }
+            
         }
     }
     
--- src/lab2p2_josuerivera/Usuario.java
+++ src/lab2p2_josuerivera/Usuario.java
@@ -10,7 +10,37 @@ package lab2p2_josuerivera;
  */
 public class Usuario {
     private String name;
-    private String contrasenna;
+    private String contraseña;
     private String Tipo_User;
+
+    public Usuario(String name, String contraseña, String Tipo_User) {
+        this.name = name;
+        this.contraseña = contraseña;
+        this.Tipo_User = Tipo_User;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTipo_User() {
+        return Tipo_User;
+    }
+
+    public void setTipo_User(String Tipo_User) {
+        this.Tipo_User = Tipo_User;
+    }
     
 }
